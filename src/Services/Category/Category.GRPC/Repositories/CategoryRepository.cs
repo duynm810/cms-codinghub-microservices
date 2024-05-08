2ef3f27502b78b7fd94b945fcfcae2fcf28e3e9c
Duy Nguyen <duynm@unit.com.vn>
2024-05-08 17:34:01 +0700

    - Add repository

--- src/Services/Category/Category.GRPC/Repositories/CategoryRepository.cs
+++ src/Services/Category/Category.GRPC/Repositories/CategoryRepository.cs
@@ -1,8 +1,15 @@
+using Category.GRPC.Entities;
+using Category.GRPC.Persistence;
 using Category.GRPC.Repositories.Interfaces;
+using Infrastructure.Domains.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Category.GRPC.Repositories;
 
-public class CategoryRepository : ICategoryRepository
+public class CategoryRepository(CategoryContext dbContext) :  RepositoryQueryBase<CategoryBase, long, CategoryContext>(dbContext), ICategoryRepository
 {
-    
+    public async Task<bool> HasPost(long categoryId)
+    {
+        return await FindByCondition(x => x.Id == categoryId).AnyAsync();
+    }
 }
--- src/Services/Category/Category.GRPC/Repositories/Interfaces/ICategoryRepository.cs
+++ src/Services/Category/Category.GRPC/Repositories/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,10 @@
+using Category.GRPC.Entities;
+using Category.GRPC.Persistence;
+using Contracts.Domains.Repositories;
+
 namespace Category.GRPC.Repositories.Interfaces;
 
-public interface ICategoryRepository
+public interface ICategoryRepository : IRepositoryQueryBase<CategoryBase, long, CategoryContext>
 {
-    
+    Task<bool> HasPost(long categoryId);
 }
