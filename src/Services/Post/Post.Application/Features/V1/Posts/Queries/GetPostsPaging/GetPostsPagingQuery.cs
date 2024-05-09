9e1b8c3c28fbedbab8a251ae00ef8bf0b7bf6b8e
Duy Nguyen <duynguyendev810@gmail.com>
2024-05-09 22:48:21 +0700

    - Refactor source code

--- src/Services/Post/Post.Application/Features/V1/Posts/Queries/GetPostsPaging/GetPostsPagingQuery.cs
+++ src/Services/Post/Post.Application/Features/V1/Posts/Queries/GetPostsPaging/GetPostsPagingQuery.cs
@@ -4,7 +4,7 @@ using Shared.Responses;
 
 namespace Post.Application.Features.V1.Posts.Queries.GetPostsPaging;
 
-public class GetPostsPagingQuery(int pageNumber = 1, int pageSize = 10) : IRequest<ApiResult<PagedResponse<PostDto>>>
+public class GetPostsPagingQuery(int pageNumber, int pageSize) : IRequest<ApiResult<PagedResponse<PostDto>>>
 {
     public int PageNumber { get; set; } = pageNumber;
 
