b6b5e3dde944cff44ea8a7d0d9fdb496510ffd45
Duy Nguyen <duynguyendev810@gmail.com>
2024-05-11 22:24:27 +0700

    - Commit missing file

--- src/Services/Post/Post.Api/Program.cs
+++ src/Services/Post/Post.Api/Program.cs
@@ -12,7 +12,7 @@ var configuration = builder.Configuration;
 
 // Initialize console logging for application startup
 Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
-Log.Information("Start {ApplicationName} up", builder.Environment.ApplicationName);
+Log.Information("Starting up {ApplicationName}", builder.Environment.ApplicationName);
 
 try
 {
