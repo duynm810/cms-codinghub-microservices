d549ebe60508c2ed9bc19de92c98f68601c09028
Duy Nguyen <duynguyendev810@gmail.com>
2024-05-19 15:07:40 +0700

    - Refactor ApplicationExtensions.cs for HealthChecks UI

--- src/WebApps/HealthCheck/WebApps.HealthCheck/Program.cs
+++ src/WebApps/HealthCheck/WebApps.HealthCheck/Program.cs
@@ -25,27 +25,8 @@ try
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    // Add health check UI middleware
-    app.MapHealthChecksUI();
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+    // Set up middleware and request handling pipeline
+    app.ConfigurePipeline();
 
     app.Run();
 }
