8d0ba59c7e1f1940f94cdff708e5bc1296035d95
Duy Nguyen <duynguyendev810@gmail.com>
2024-05-26 23:17:19 +0700

    - Refactor Header view component

--- src/WebApps/UI/WebApps.UI/Components/HeaderViewComponent.cs
+++ src/WebApps/UI/WebApps.UI/Components/HeaderViewComponent.cs
@@ -9,18 +9,26 @@ public class HeaderViewComponent(ICategoryApiClient categoryApiClient, ILogger l
 {
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        var categories = await categoryApiClient.GetCategories();
-        if (categories is { IsSuccess: true, Data: not null })
+        try
         {
-            var viewModel = new HeaderViewModel
+            var categories = await categoryApiClient.GetCategories();
+            if (categories is { IsSuccess: true, Data: not null })
             {
-                Categories = categories.Data
-            };
-            
-            return View(viewModel);
-        }
+                var viewModel = new HeaderViewModel
+                {
+                    Categories = categories.Data
+                };
 
-        logger.Error("Failed to load categories or no categories found.");
-        return Content("No categories found.");
+                return View(viewModel);
+            }
+
+            logger.Error("Failed to load categories or no categories found.");
+            return Content("No categories found.");
+        }
+        catch (Exception e)
+        {
+            logger.Error("{MethodName}. Message: {ErrorMessage}", nameof(InvokeAsync), e);
+            return Content("Unable to load categories at this time.");
+        }
     }
 }
