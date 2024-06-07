7d3e1ce7264a5183068fd17b0d9fa6722e90a21d
Duy Nguyen <duynm@unit.com.vn>
2024-06-07 16:21:15 +0700

    - Fix structure folder

---
+++ src/Services/PostInTag/PostInTag.Api/GrpcServices/Interfaces/IPostGrpcService.cs
@@ -0,0 +1,9 @@
+using Shared.Dtos.PostInSeries;
+using Shared.Dtos.PostInTag;
+
+namespace PostInTag.Api.GrpcServices.Interfaces;
+
+public interface IPostGrpcService
+{
+    Task<IEnumerable<PostInTagDto>> GetPostsByIds(IEnumerable<Guid> ids);
+}
---
+++ src/Services/PostInTag/PostInTag.Api/MappingProfile.cs
@@ -0,0 +1,10 @@
+using AutoMapper;
+
+namespace PostInTag.Api;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+    }
+}
