b702cf88dcbc4507f7aa9ba535207016443e9e8f
Duy Nguyen <duynguyendev810@gmail.com>
2024-06-15 00:57:16 +0700

    - Completed comment feature

--- src/WebApps/UI/WebApps.UI/wwwroot/js/controllers/posts.controller.js
+++ src/WebApps/UI/WebApps.UI/wwwroot/js/controllers/posts.controller.js
@@ -31,52 +31,99 @@ const postsController = function () {
     }
 
     function registerEvents() {
-        console.log('Registering events'); // Kiểm tra việc đăng ký sự kiện
+        console.log('Registering events');
+
+        // Handle submission of new comment form (Xử lý submit form bình luận mới)
         $("#commentform").submit(function (e) {
             e.preventDefault();
-            console.log('Comment form submitted'); // Kiểm tra việc submit form
+
+            // Check form submisstion (Kiểm tra việc submit form)
+            console.log('Comment form submitted');
 
             const form = $(this);
             const url = form.attr('action');
-            console.log('Submitting form to URL:', url); // Kiểm tra URL
+
+            // Check url (Kiểm tra URL)
+            console.log('Submitting form to URL:', url);
 
             $.post(url, form.serialize()).done(function (response) {
-                console.log('Form submission successful:', response); // Kiểm tra phản hồi
+                // Check response (Kiểm tra phản hồi)
+                console.log('Form submission successful:', response);
 
+                // Generate the HTML for the new comment and add it to the comments list (Tạo HTML cho bình luận mới và thêm vào danh sách bình luận)
                 const content = $("#txt_new_comment_content").val();
                 const newCommentHtml = generateCommentHtml(response.data.id, content, new Date(), $('#hid_current_login_name').val());
 
+                // Reset form and update interface (Reset form và cập nhật giao diện)
                 $("#txt_new_comment_content").val('');
                 $('#comment_list').prepend(newCommentHtml);
+                
                 const numberOfComments = parseInt($('#hid_number_comments').val()) + 1;
                 $('#hid_number_comments').val(numberOfComments);
                 $('#comments-title').text('Các bình luận (' + numberOfComments + ')');
             });
         });
 
+        // Handle when the user clicks the "Reply" button (Xử lý khi người dùng nhấp vào nút "Reply")
         $('body').on('click', '.comment-reply-link', function (e) {
             e.preventDefault();
-            console.log('Reply link clicked'); // Kiểm tra việc click vào reply link
 
+            // Check for clicking on the reply link (Kiểm tra việc click vào reply link)
+            console.log('Reply link clicked');
+
+            // Display comment reply form (Hiển thị form trả lời bình luận)
             const commentId = $(this).data('commentid');
             const replyFormHtml = generateReplyFormHtml(commentId);
 
             $('#reply_comment_' + commentId).html(replyFormHtml);
 
+            // Set a timeout to hide the reply form if there is no input after 10 seconds (Thiết lập thời gian chờ để ẩn form trả lời nếu không có nhập liệu sau 10 giây)
+            let replyTimeout = setTimeout(function() {
+                if ($('#txt_reply_content_' + commentId).val().trim() === '') {
+                    $('#reply_comment_' + commentId).html('');
+                }
+            }, 5000); // 10 giây
+
+            // If the user starts typing, cancel the timeout (Nếu người dùng bắt đầu nhập, hủy bỏ thời gian chờ)
+            $('#txt_reply_content_' + commentId).on('input', function() {
+                clearTimeout(replyTimeout);
+            });
+
+            // Nếu người dùng rời khỏi textarea mà không nhập gì, thiết lập thời gian chờ để ẩn form
+            $('#txt_reply_content_' + commentId).on('blur', function() {
+                replyTimeout = setTimeout(function() {
+                    if ($('#txt_reply_content_' + commentId).val().trim() === '') {
+                        $('#reply_comment_' + commentId).html('');
+                    }
+                }, 5000); // 10 giây
+            });
+
+            // Nếu người dùng quay lại textarea, hủy bỏ thời gian chờ
+            $('#txt_reply_content_' + commentId).on('focus', function() {
+                clearTimeout(replyTimeout);
+            });
+
+            // Handle submit form to reply to comments (Xử lý submit form trả lời bình luận)
             $("#frm_reply_comment_" + commentId).submit(function (e) {
                 e.preventDefault();
-                console.log('Reply form submitted for commentId:', commentId); // Kiểm tra việc submit form trả lời
+
+                // Check the submission of the answer form (Kiểm tra việc submit form trả lời)
+                console.log('Reply form submitted for commentId:', commentId);
 
                 const form = $(this);
                 const url = form.attr('action');
-                console.log('Submitting reply form to URL:', url); // Kiểm tra URL
+
+                // Check url (Kiểm tra URL)
+                console.log('Submitting reply form to URL:', url);
 
                 $.post(url, form.serialize()).done(function (response) {
-                    console.log('Reply form submission successful:', response); // Kiểm tra phản hồi
+                    // Kiểm tra phản hồi
+                    console.log('Reply form submission successful:', response);
 
                     const content = $("#txt_reply_content_" + commentId).val();
                     const newReplyHtml = generateReplyHtml(response.data.id, content, new Date(), $('#hid_current_login_name').val());
 
+                    // Reset form and update interface (Reset form và cập nhật giao diện)
                     $("#txt_reply_content_" + commentId).val('');
                     $('#reply_comment_' + commentId).html('');
                     $('#children_comments_' + commentId).prepend(newReplyHtml);
@@ -88,7 +135,7 @@ const postsController = function () {
             });
         });
     }
-
+    
     function formatRelativeTime(fromDate) {
         if (fromDate === undefined)
             fromDate = new Date();
@@ -157,10 +204,7 @@ const postsController = function () {
                     <div class="d-flex justify-content-between">
                         <div class="d-flex align-items-center">
                             <h5><a href="#">${userId}</a></h5>
-                            <p class="date">${formatRelativeTime(createdDate)}</p>
-                        </div>
-                        <div class="reply-btn">
-                            <a href="#" class="btn-reply comment-reply-link" data-commentid="${id}">Reply</a>
+                            <p class="date">${formatRelativeTime(new Date(createdDate))}</p>
                         </div>
                     </div>
                 </div>
@@ -172,18 +216,20 @@ const postsController = function () {
     function generateReplyFormHtml(commentId) {
         return `
         <div class="comment-form form-contact rounded bordered">
-            <form action="/post/addNewComment" id="frm_reply_comment_${commentId}" class="comment-form" method="post">
+            <form action="/posts/addNewComment" id="frm_reply_comment_${commentId}" class="comment-form" method="post">
                 <input type="hidden" name="postId" value="${$('#hid_post_id').val()}" />
                 <input type="hidden" name="replyId" value="${commentId}" />
                 <div class="messages"></div>
                 <div class="row">
                     <div class="column col-md-12">
-                        <div class="form-group">
-                            <textarea name="content" id="txt_reply_content_${commentId}" class="form-control" rows="4" placeholder="Bạn muốn bình luận gì á..." required="required"></textarea>
+                        <div class="form-group d-flex align-items-center">
+                            <textarea name="content" id="txt_reply_content_${commentId}" class="form-control" rows="2" placeholder="Xin nhập bình luận..." required="required"></textarea>
+                            <button type="submit" id="btn_send_reply" class="btn btn-default ml-2">
+                               <i class="fas fa-paper-plane"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <input type="submit" name="submit" id="btn_send_reply" class="btn btn-default" value="Gửi"/>
             </form>
         </div>
         `;
