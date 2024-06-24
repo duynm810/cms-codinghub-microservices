7d1903346dead716f34d001064d20d2378e5b1d3
Duy Nguyen <duynm@unit.com.vn>
2024-06-24 15:01:53 +0700

    - Refactor create post and update post

---
+++ src/WebApps/UI/WebApps.UI/wwwroot/js/controllers/accounts.controller.js
@@ -0,0 +1,116 @@
+const accountsController = function () {
+    this.initialize = function () {
+
+        this.uploadFileHandlers();
+
+        // Auto-generate slug from title
+        document.querySelector('input[name="Title"]').addEventListener('input', function () {
+            let titleValue = this.value;
+            document.querySelector('input[name="Slug"]').value = createSlug(titleValue);
+        });
+    }
+
+    this.uploadFileHandlers = function () {
+        const fileInput = document.getElementById("thumbnail");
+        const dropArea = document.getElementById("drop-area");
+        const fileList = document.getElementById("file-list");
+
+        if (!fileInput || !dropArea || !fileList) return;
+
+        dropArea.addEventListener("dragover", (event) => {
+            event.preventDefault();
+            dropArea.classList.add("drag-over");
+        });
+
+        dropArea.addEventListener("dragleave", () => {
+            dropArea.classList.remove("drag-over");
+        });
+
+        dropArea.addEventListener("drop", async (event) => {
+            event.preventDefault();
+            dropArea.classList.remove("drag-over");
+            await this.handleFiles(event.dataTransfer.files);
+        });
+
+        fileInput.addEventListener("change", async () => {
+            await this.handleFiles(fileInput.files);
+        });
+
+        this.handleFiles = async (files) => {
+            const fileList = document.getElementById("file-list");
+            fileList.innerHTML = '';
+            [...files].forEach(file => {
+                const fileItem = document.createElement("div");
+                fileItem.className = "file-item";
+
+                const fileThumbnail = document.createElement("div");
+                fileThumbnail.className = "file-thumbnail";
+                const img = document.createElement("img");
+                img.src = URL.createObjectURL(file);
+                img.alt = "Thumbnail";
+                fileThumbnail.appendChild(img);
+
+                const fileDetails = document.createElement("div");
+                fileDetails.className = "file-details";
+                fileDetails.innerHTML = `
+                    <span class="file-name">${file.name}</span>
+                    <span class="file-size">${(file.size / 1024 / 1024).toFixed(2)} MB</span>
+                `;
+
+                const fileRemove = document.createElement("div");
+                fileRemove.className = "file-remove";
+                const removeButton = document.createElement("button");
+                removeButton.type = "button";
+                removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+                removeButton.addEventListener("click", () => {
+                    this.showConfirm(
+                        'Are you sure you want to delete this image?',
+                        'This action cannot be undone.',
+                        'Yes, delete it',
+                        'Cancel',
+                        () => {
+                            fileInput.value = '';
+                            fileList.innerHTML = '';
+                            document.getElementById('thumbnailUrl').value = ''; // Xóa URL hình ảnh khi xóa file
+                        }
+                    );
+                });
+
+                fileRemove.appendChild(removeButton);
+                fileItem.appendChild(fileThumbnail);
+                fileItem.appendChild(fileDetails);
+                fileItem.appendChild(fileRemove);
+                fileList.appendChild(fileItem);
+            });
+
+            await this.uploadFiles(files);
+        }
+
+        this.uploadFiles = async (files) => {
+            const formData = new FormData();
+            formData.append('file', files[0]);
+            formData.append('type', 'image');
+
+            try {
+                const response = await fetch('/api/media/upload', {
+                    method: 'POST',
+                    body: formData
+                });
+
+                const responseBody = await response.text();
+                if (!response.ok) {
+                    this.showError(`Upload failed with status: ${response.status}, error: ${responseBody}`);
+                    return;
+                }
+
+                const result = JSON.parse(responseBody);
+                console.log(result);
+
+                // Update the hidden input with the URL of the uploaded file
+                document.getElementById('thumbnailUrl').value = result.data;
+            } catch (error) {
+                this.showError(`Error uploading file: ${error.message}`);
+            }
+        }
+    }
+}
