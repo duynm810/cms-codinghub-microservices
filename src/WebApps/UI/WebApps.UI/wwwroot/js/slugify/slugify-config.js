58dd38c8b8efcce310306f52941dfe9016fc56b6
Duy Nguyen <duynguyendev810@gmail.com>
2024-07-05 00:33:59 +0700

    - Add region for js file

--- src/WebApps/UI/WebApps.UI/wwwroot/js/slugify/slugify-config.js
+++
@@ -1,7 +0,0 @@
-// Slugify function for both English and Vietnamese
-function createSlug(value) {
-    return slugify(value, {
-        lower: true,
-        locale: 'vi' // Supports Vietnamese language
-    });
-}
