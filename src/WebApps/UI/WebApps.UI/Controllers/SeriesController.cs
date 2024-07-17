827f02150a5d8e0024fcc0756efebc69e5afff39
Duy Nguyen <duynm@unit.com.vn>
2024-07-17 11:59:47 +0700

    - Completed UI add to series for post

--- src/WebApps/UI/WebApps.UI/Controllers/SeriesController.cs
+++ src/WebApps/UI/WebApps.UI/Controllers/SeriesController.cs
@@ -1,16 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApps.UI.ApiClients.Interfaces;
+using WebApps.UI.Models.Series;
 
 namespace WebApps.UI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class SeriesController(ISeriesApiClient seriesApiClient) : ControllerBase
+public class SeriesController(ISeriesApiClient seriesApiClient) : Controller
 {
     [HttpGet]
     public async Task<IActionResult> GetSeries()
     {
-        var response = await seriesApiClient.GetSeries();
-        return Ok(response);
+        try
+        {
+            var response = await seriesApiClient.GetSeries();
+            if (response is { IsSuccess: true, Data: not null })
+            {
+                var items = new SeriesViewModel()
+                {
+                    Series = response.Data
+                };
+                
+                return PartialView("Partials/Accounts/_AddPostsToSeriesPartial", items);
+            }
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+
+        return Json(new { success = false });
     }
 }
