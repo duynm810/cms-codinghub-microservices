4ebd8ec3d41451bdfffa116bfe3a723c31defd94
Duy Nguyen <duynm@unit.com.vn>
2024-07-30 13:34:17 +0700

    - Fix some bug in quill editor

--- src/WebApps/UI/WebApps.UI/Helpers/ImageUrlTagHelper.cs
+++ src/WebApps/UI/WebApps.UI/Helpers/ImageUrlTagHelper.cs
@@ -7,19 +7,28 @@ namespace WebApps.UI.Helpers;
 public class ImageUrlTagHelper(IOptions<ApiSettings> apiSettings) : TagHelper
 {
     private readonly ApiSettings _apiSettings = apiSettings.Value;
-    
+
+    [HtmlAttributeName("src")] 
     public string? Src { get; set; }
+
+    [HtmlAttributeName("id")] 
     public string? Id { get; set; }
-    
+
+    [HtmlAttributeName("class")]
+    public string? AdditionalClass { get; set; }
+
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         var fullUrl = !string.IsNullOrEmpty(_apiSettings.Port)
-            ? $"{_apiSettings.ServerUrl}:{_apiSettings.Port}/{Src}" 
+            ? $"{_apiSettings.ServerUrl}:{_apiSettings.Port}/{Src}"
             : $"{_apiSettings.ServerUrl}/{Src}";
 
         output.TagName = "img"; // Replace <image-url> with <img> tag
         output.Attributes.SetAttribute("src", fullUrl);
-        output.Attributes.SetAttribute("class", "border-radius-5");
+
+        var classValue = $"{AdditionalClass}";
+      
+        output.Attributes.SetAttribute("class", classValue);
 
         if (!string.IsNullOrEmpty(Id))
         {
